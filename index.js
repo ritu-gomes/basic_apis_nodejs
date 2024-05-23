a14fb31e5f24995c9ba8c24aed6cb48a52094dd5
ritu <irengomes018@gmail.com>
2024-05-24 00:39:12 +0600

    using try catch instead of then catch

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const app = express();
+const usersRouter = require('./routes/users');
 const {userSchema} = require('./userSchema');
 
 app.use(express.json());
-const userData = [
-    {
-        "id": "1",
-        "name": "Sara",
-        "email_or_phone": "sara123@gmail.com",
-        "password": "laal33&"
-    },
-    {
-        "id": "2",
-        "name": "elsa",
-        "email_or_phone": "elsa55@gmail.com",
-        "password": "kfg33&"
-    }
-];
+app.use('/users', usersRouter);
+
 const productData = [
     {
         "id": "1",
@@ -31,72 +20,6 @@ const productData = [
         "color": "blue"
     },
 ];
-
-// const validateUser = (user) => {
-//     userSchema.validate(user)
-//     .then((res) => {
-//         return null;
-//     })
-//     .catch((err) => {
-//         return err.arrors[0];
-//     })
-// }
-
-
-app.get("/users", (req, res) => {
-    res.status(201).send(userData);
-}); 
-
-app.post("/users", async (req, res) => {
-    const { id, name, email_or_phone, password } = req.body;
-
-    userSchema.validate({name, email_or_phone})
-    .then((result) => {
-        const newUser = {
-            id: id,
-            name: name,
-            email_or_phone: email_or_phone,
-            password: password
-        };
-        userData.push(newUser);
-        res.status(201).send(userData);
-    })
-    .catch(err => {
-        return res.status(400).send(err.errors[0]);
-    });
-
-});
-
-app.delete("/users/:id", (req, res) => {
-    const id = req.params.id;
-    const theUser = userData.find(user => user.id == id);
-    // console.log(theUser);
-    if(!theUser){
-        return res.status(404).send("user not found");
-    };
-    const userIndex = userData.indexOf(theUser);
-    // console.log(userIndex);
-    userData.splice(userIndex,1);
-    res.send(userData);
-});
-
-app.put("/users/:id", (req, res) => {
-    const id = req.params.id;
-    const { name, email_or_phone, password } = req.body;
-
-    const theUser = userData.find(user => user.id == id);
-
-    if (!theUser) {
-        return res.status(404).send("user not found");
-    };
-
-    if(name){theUser.name = name};
-    if(email_or_phone){theUser.email_or_phone = email_or_phone};
-    if(password){theUser.password = password};
-    // theUser = { ...theUser, name, email_or_phone, password};
-
-    res.status(201).send(userData);
-});
 
 
 app.get("/products", (req, res) => {
